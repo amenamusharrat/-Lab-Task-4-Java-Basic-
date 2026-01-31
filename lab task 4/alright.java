967a6a015e1147dd4dc3eaf955109f0174fa40f5
amenamusharrat <amenamusharrat1102@gmail.com>
2026-01-31 17:24:44 +0600

    Add files via upload

---
+++ lab task 4/alright.java
@@ -0,0 +1,6 @@
+public class alright {
+  public static void main(String[] args) {
+    String txt = "It\'s alright.";
+    System.out.println(txt);
+  }
+}
---
+++ lab task 4/greater.java
@@ -0,0 +1,10 @@
+public class greater {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 9;
+
+    boolean isGreater = x > y;
+
+    System.out.println(isGreater); // Outputs true
+  }
+}
